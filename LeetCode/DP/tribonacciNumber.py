dca1f4c5dcaa7c3a3cc6ec13f08b4cc504c878cc
HalitBatuhanASLAN <halitbatuhanaslan@gmail.com>
2026-01-06 14:45:46 +0300

    tribonacci added

---
+++ LeetCode/DP/tribonacciNumber.py
@@ -0,0 +1,16 @@
+'''
+https://leetcode.com/problems/n-th-tribonacci-number?envType=problem-list-v2&envId=dynamic-programming
+'''
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0: return 0
+        if n == 1: return 1
+        if n == 2: return 1
+        dp = [0] *(n+1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        return dp[n]
